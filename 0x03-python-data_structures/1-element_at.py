170f6b5143a646048a482ba6baae877940f352d6
Robelhail <richnrob.2020@gmail.com>
2023-03-13 00:50:24 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    listlength = len(my_list) - 1
-    if (idx < 0 or idx > listlength):
-        return (None)
-    else:
-        return (my_list[idx])
+# 0-print_list_integer.py
+def print_list_integer(my_list=[]):
+    """Print all integerson the list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
