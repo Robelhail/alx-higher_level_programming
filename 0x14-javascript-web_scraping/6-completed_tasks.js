5534a5d123794ae43148169576d069aacd088602
Robelhail <richnrob.2020@gmail.com>
2023-07-25 10:53:29 -0700

    again

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,45 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-// The first argument is the API URL
-const baseURL = process.argv[2];
-request(baseURL, (error, response, body) => {
-  const aggregate = {};
+const apiUrl = process.argv[2];
+
+if (!apiUrl) {
+  console.log('Usage: node 6-completed_tasks.js <API_URL>');
+  process.exit(1);
+}
+
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('Error while fetching data:', error);
+    process.exit(1);
   }
-  const json = JSON.parse(body);
-  json.forEach(element => {
-    if (element.completed) {
-      if (!aggregate[element.userId]) {
-        aggregate[element.userId] = 0;
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data. Status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  try {
+    const tasks = JSON.parse(body);
+
+    // Filter out completed tasks
+    const completedTasks = tasks.filter((task) => task.completed);
+
+    // Count completed tasks for each user
+    const completedTasksByUser = completedTasks.reduce((acc, task) => {
+      if (acc[task.userId]) {
+        acc[task.userId]++;
+      } else {
+        acc[task.userId] = 1;
       }
-      aggregate[element.userId]++;
-    }
-  });
-  console.log(aggregate);
+      return acc;
+    }, {});
+
+    console.log(completedTasksByUser);
+  } catch (parseError) {
+    console.error('Error while parsing data:', parseError);
+    process.exit(1);
+  }
 });
+
